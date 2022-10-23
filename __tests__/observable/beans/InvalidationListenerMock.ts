3ee04df8f2130b096d4176cc73554ed5e56f8799
Vincent Girard <vinccool96@gmail.com>
2022-10-23 17:31:27 -0400

    Start tests

---
+++ __tests__/observable/beans/InvalidationListenerMock.ts
@@ -0,0 +1,30 @@
+import { InvalidationListener, Observable } from "../../../src/observable/beans"
+
+export class InvalidationListenerMock implements InvalidationListener {
+
+  private observable: Observable | null
+
+  private counter: number
+
+  constructor() {
+    this.observable = null
+    this.counter = 0
+  }
+
+  invalidated(observable: Observable): void {
+    this.observable = observable
+    this.counter++
+  }
+
+  reset() {
+    this.observable = null
+    this.counter = 0
+  }
+
+  check(observable: Observable, counter: number) {
+    expect(this.observable).toBe(observable)
+    expect(this.counter).toEqual(counter)
+    this.reset()
+  }
+
+}
