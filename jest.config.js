5630078ddd6a16c13ef3991102e72439b17ac966
Vincent Girard <vinccool96@gmail.com>
2022-12-06 11:20:57 -0500

    Moved

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   testMatch: ["**/*.test.ts"],
   transform: { "^.+\\.ts$": "ts-jest" },
   moduleDirectories: ["node_modules"],
-  globalSetup: "./__tests__/config/globalSetup.ts",
+  globalSetup: "./tests/config/globalSetup.ts",
   verbose: true,
 }
